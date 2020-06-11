96141acef61f483cc4ee350fe3dc9f99154f3e52
andzajan <andzajan@gmail.com>
2020-06-11 16:54:37 +0100

    No need to load qcrms package.

--- inst/scripts/xcms_processing_xcmsSet.R
+++ inst/scripts/xcms_processing_xcmsSet.R
@@ -1,5 +1,4 @@
 library(xcms)
-library(qcrms)
 
 ## Get the full path to the mzML files
 mzml_files <- dir(system.file("extdata/MTBLS404/mzML", package="qcrmsData"),
@@ -9,4 +8,4 @@ xset <- xcms::xcmsSet(mzml_files, method="centWave", peakwidth=c(7, 30),
     ppm=8, mzdiff=0.013, snthresh=10, noise=5000)
 MTBLS404_xset <- xcms::group(xset, method="density", bw=5, mzwid=0.01)
 
-usethis::use_data(MTBLS404_xset)
+usethis::use_data(MTBLS404_xset, overwrite=T)
